7cc62c728d3f5d682b18bc7cc906efb68c1cb4ec
Phong Phú <phongphu.official@gmail.com>
2021-02-22 11:22:24 +0700

    connectToDatabase

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const server = require('./src/server');
 const config = require('./src/config');
+const Database = require('./src/database');
 
 const http = require('http').createServer(server);
 const io = require('socket.io')(http);
 
-http.listen(config.PORT, config.HOST, function () {
-    console.log(`${config.HOST}:${config.PORT}`);
-})
+
+const connectionString = 'mongodb+srv://admin:admin@cluster0.9grd8.mongodb.net/chat_DB?retryWrites=true&w=majority'
+
+
+async function main() {
+
+    await Database.instance.connectToMongoDB(connectionString)
+    http.listen(config.PORT, config.HOST, function () {
+        console.log(`${config.HOST}:${config.PORT}`);
+    })
+}
+
+main();
+
+
 
 
---
+++ server/src/database.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const index = require('../index')
+
+class Database {
+    /**
+     * @type {Database}
+     */
+    static _cache = null;
+
+    static get instance() {
+        if (this._cache == null) {
+            return this._cache = new Database();
+        }
+        return this._cache;
+    }
+
+    async connectToMongoDB(connectionString) {
+        return new Promise((resolve, reject) => {
+            mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+            const db = mongoose.connection;
+            db.on("error", (err) => {
+                reject(err);
+            });
+
+            db.once('open', function () {
+                console.log('Connection Successfully!');
+                resolve(db);
+            });
+        });
+    }
+
+}
+
+
+
+module.exports = Database;
