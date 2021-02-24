683d7cae4ea3ad2814a5a43b89705c94c13b1976
trung <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-02-24 11:40:04 +0700

    error

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  user: any;
+  constructor(private auth: AngularFireAuth) { }
+  public provider = new firebase.default.auth.GoogleAuthProvider();
+  public async login() {
+    try {
+      await this.auth.signInWithPopup(this.provider);
+      alert('Đăng nhập thành công');
+    } catch (err) {
+      console.log(err);
+    }
   }
 
+  public async logout() {
+    try {
+      await this.auth.signOut();
+      this.user = null;
+      alert('Đăng xuất thành công');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  ngOnInit(): void {
+    this.auth.authState.subscribe((hello) => {
+      if (hello) {
+        this.user = hello;
+      }
+    });
+  }
 }
